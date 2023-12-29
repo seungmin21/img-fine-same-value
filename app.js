6822cffec552f04c22b80cc85317bdacb0bb2b79
ohseungmin <ohmin2886@gmail.com>
2023-12-30 00:45:08 +0900

    POST요청으로 log.json 기록 저장

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post('/addValue', (req, res) => {
+  const { value } = req.body;
+
+  if (!value) {
+    return res.status(400).send('Invalid value');
+  }
+
+  fs.readFile('log.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error reading log file');
+    }
+
+    let log = [];
+    if (data) {
+      try {
+        log = JSON.parse(data);
+      } catch (parseError) {
+        console.error(parseError);
+        return res.status(500).send('Error parsing log file');
+      }
+    }
+
+    log.push(value); // 새 값 추가
+
+    fs.writeFile('log.json', JSON.stringify(log), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Error writing to log file');
+      }
+
+      res.status(200).send('Value added successfully');
+    });
+  });
+});
+
 
 app.listen(port, () => {
-  console.log(`서버 http://localhost:${port} 에서 실행 중입니다.`)
-})
+  console.log(`서버 http://localhost:${port} 에서 실행 중입니다.`);
+});
