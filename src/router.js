c842e3ad9b229dfe6f559d494fb1a75a2b21aa61
ohseungmin <ohmin2886@gmail.com>
2024-01-30 12:31:09 +0900

    image 객체를 json 파일에 넣고 라이터 처리를 통해 응답하는 것으로 변경

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 
+// index.html을 읽어서 get요청을 하는 로직
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+// 클라이언트 파일에서 서버로 post 요청을 진행하는 로직
 router.post('/addValue', (req, res) => {
   const { value } = req.body;
 
@@ -44,10 +46,16 @@ router.post('/addValue', (req, res) => {
 });
 
 // fs.readFilSync(path(분석할 매체), option(옵션))
-const jsonData = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+const jsonData = JSON.parse(fs.readFileSync('./src/data.json', 'utf8'))
+const jsonImageData = JSON.parse(fs.readFileSync('./src/ImageData.json', 'utf8'))
 
+// src/data.json파일을 조회할 로직
 router.get('/getData', (req, res) => {
   res.json(jsonData);
 });
+
+router.get('/getImage', (req, res) => {
+  res.json(jsonImageData)
+})
 
 module.exports = router;
