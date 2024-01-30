96b95e8922ba3874221095966e6f8795f365e7a4
ohseungmin <ohmin2886@gmail.com>
2024-01-30 17:14:52 +0900

    express 서버 파일 이름 변경 cors 라이브러리 설치

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
 const router = require('./src/router')
-const PORT = process.env.PORT || 3000;
+const cors = require('cors')
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 
 // static으로 정적 파일 제공(이미지파일)
-app.use('/cat-image', express.static(path.join(__dirname, 'cat-image')));
+app.use(express.static(path.join(__dirname, '..', 'src', 'cat-image')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 라우터 설정
-app.use('*', router);
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`서버 http://localhost:${PORT} 에서 실행 중입니다.`);
