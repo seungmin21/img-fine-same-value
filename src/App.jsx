fadd1d55c88f7f556bbd025312e76b6c84d08916
ohseungmin <ohmin2886@gmail.com>
2024-01-31 17:13:07 +0900

    저장되고 있는 log.json의 기록을 브라우저에서 출력하는 로직 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import cat_names from "./nameValue";
 
@@ -9,6 +9,7 @@ function App() {
   const [inputText, setInputText] = useState("");
   const [imagePath, setImagePath] = useState(null);
   const [randomItem, setRandomItem] = useState(null);
+  const [logData, setLogData] = useState([]);
 
   // onClick을 사용하기 위한 함수
   // 랜덤으로 alert에 배열 중에 랜덤으로 요소를 출력
@@ -45,16 +46,45 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/api/logData");
+        const data = await response.json();
+        setLogData(data);
+      } catch (error) {
+        console.error("Error fetching log data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div id="Instead">
+      <div id="Log-Mark">
+      {logData.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </div>
       <div id="container-one">
         <button id="text-name" onClick={handleClick}>
           Hint
         </button>
-        <input type="text" id="search-bar" name="search-bar" placeholder="입력해주세요." value={inputText} onChange={handleChange} onKeyPress={handleKeyPress}
+        <input
+          type="text"
+          id="search-bar"
+          name="search-bar"
+          placeholder="입력해주세요."
+          value={inputText}
+          onChange={handleChange}
+          onKeyPress={handleKeyPress}
         />
       </div>
       <div id="container-two">
+        <h3 className="marginLeft marginBottom">
+          원하는 사진이 나오지 않을 수도 있습니다.
+        </h3>
         {/* 이미지가 출력될 태그 */}
         <div id="result">
           {imagePath && <img src={imagePath} alt="?" />}
