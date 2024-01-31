049706afa93228260b35ef0b421bc35576c52515
ohseungmin <ohmin2886@gmail.com>
2024-01-31 14:22:46 +0900

    배열에 모듈화를 진행해서 클라이언트 파일에 배열을 작성하지 않는 방향으로 진행

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { useState } from "react";
 import axios from "axios";
+import cat_names from "./nameValue";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,109 +9,9 @@ function App() {
   const [inputText, setInputText] = useState("");
   const [imagePath, setImagePath] = useState(null);
   const [randomItem, setRandomItem] = useState(null);
-  const cat_names = [
-    "아비니시안",
-    "에게안",
-    "아메리칸 밥테일",
-    "아메리칸 컬",
-    "아메리칸 링테일",
-    "아나톨리",
-    "키프로스",
-    "키프로스 아프로디테",
-    "아라비안 마우",
-    "아시안",
-    "티파니",
-    "발리니즈",
-    "밤비노",
-    "뱅갈",
-    "버먼",
-    "봄베이",
-    "브라질리안 쇼트헤어",
-    "브리티시 롱헤어",
-    "브리티시 쇼트헤어",
-    "버미즈",
-    "버밀라",
-    "캘리포니아 스팽글드",
-    "세일론",
-    "샹틸리 티파니",
-    "샤르트뢰",
-    "쵸시",
-    "치토",
-    "컬러포인트 쇼트헤어",
-    "데본 렉스",
-    "돈스코이",
-    "드웰프",
-    "이집션 마우",
-    "유러피안 쇼트헤어",
-    "이그저틱 롱헤어",
-    "폴덱스",
-    "포린 화이트",
-    "제네타",
-    "저먼 렉스",
-    "하바나 브라운",
-    "하일랜더",
-    "히말라얀",
-    "재그",
-    "재패니즈 밥테일",
-    "자바니즈",
-    "카나니",
-    "카렐리안 밥테일",
-    "캬슈미르",
-    "카오 마니",
-    "킨카로우",
-    "쿠릴리안 밥테일",
-    "램킨",
-    "라펌",
-    "라이코이",
-    "메인쿤",
-    "만달레이",
-    "맹크스",
-    "메콩 밥테일",
-    "민스킨",
-    "먼치킨",
-    "네벨룽",
-    "네바마스커레이드",
-    "노르웨이 숲",
-    "오시캣",
-    "오호스 아즐레스",
-    "오리건 렉스",
-    "오리엔탈 바이컬러",
-    "오리엔탈 롱헤어",
-    "오리엔탈 쇼트헤어",
-    "오위히 밥",
-    "페르시안",
-    "팬서렛",
-    "피터볼드",
-    "픽시 밥",
-    "라가머핀",
-    "랙돌",
-    "라아스",
-    "러시안블루",
-    "샘 사웨트",
-    "사바나",
-    "스코티시 폴드",
-    "셀커크 렉스",
-    "세렝게티",
-    "샴",
-    "시베리안 숲",
-    "싱가푸라",
-    "스쿠쿰",
-    "스노우슈",
-    "소코케",
-    "소말리",
-    "스핑크스",
-    "수팔락",
-    "테네시 렉스",
-    "타이",
-    "통키니즈",
-    "토이밥",
-    "토이거",
-    "터키시 앙고라",
-    "터키시 반",
-    "반",
-    "요크 초콜릿",
-  ];
 
+  // onClick을 사용하기 위한 함수
+  // 랜덤으로 alert에 배열 중에 랜덤으로 요소를 출력
   const handleClick = () => {
     if (count < number) {
       const randomIndex = Math.floor(Math.random() * cat_names.length);
@@ -121,6 +22,7 @@ function App() {
     }
   };
 
+  // onChange를 사용하기 위한 함수로 상태를 바꾸는 함수
   const handleChange = (e) => {
     setInputText(e.target.value); // 입력된 텍스트를 상태에 업데이트
     setImagePath(null);
@@ -149,19 +51,14 @@ function App() {
         <button id="text-name" onClick={handleClick}>
           Hint
         </button>
-        <input
-          type="text"
-          id="search-bar"
-          name="search-bar"
-          placeholder="입력해주세요."
-          value={inputText}
-          onChange={handleChange}
-          onKeyPress={handleKeyPress}
+        <input type="text" id="search-bar" name="search-bar" placeholder="입력해주세요." value={inputText} onChange={handleChange} onKeyPress={handleKeyPress}
         />
       </div>
       <div id="container-two">
         {/* 이미지가 출력될 태그 */}
-        <div id="result">{imagePath && <img src={imagePath} alt="?" />}</div>
+        <div id="result">
+          {imagePath && <img src={imagePath} alt="?" />}
+        </div>
       </div>
     </div>
   );
