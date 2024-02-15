82207b98f78457ae3c277a5e3a08afb985074490
ohseungmin <ohmin2886@gmail.com>
2024-02-15 14:20:52 +0900

    id -> className으로 변경 80줄

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ function App() {
   return (
     <div id="Instead">
       <div className="first-container">
-      <div id="iconLogText">
+      <div className="iconLogText">
         <div className="log-maker">
           <Link to="https://github.com/seungmin21/img-fine-same-value">
           <div className="log-image"></div>
