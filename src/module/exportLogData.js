a1f6cb3ce7ba329e7c02665538940d96c041225b
ohseungmin <ohmin2886@gmail.com>
2024-02-19 17:02:23 +0900

    setInterval까지 모듈화를 진행하려고 한 모듈 파일 실험 결과는 기록 데이터 불러오기 실패

--- src/module/exportLogData.js
+++ src/module/exportLogData.js
@@ -1,14 +1,16 @@
-import fetchLogData from './fetchLogData.js'
+// 
 
-async function refreshLogData(setLogData) {
-
-  fetchLogData(setLogData);
-
-   // 2초마다 데이터 갱신
-  const intervalId = setInterval(fetchLogData, 2000);
-
-  // 컴포넌트가 언마운트되면 interval 정리
-  return () => clearInterval(intervalId);
-}
-
-export default refreshLogData
+//import fetchLogData from './fetchLogData.js'
+//
+//async function refreshLogData(setLogData) {
+//
+//  fetchLogData(setLogData);
+//
+//   // 2초마다 데이터 갱신
+//  const intervalId = setInterval(fetchLogData, 2000);
+//
+//  // 컴포넌트가 언마운트되면 interval 정리
+//  return () => clearInterval(intervalId);
+//}
+//
+//export default refreshLogData
