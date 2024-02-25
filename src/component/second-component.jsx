22be81fb996c4e8a0e5e1d37c249eecd4bc8de98
Oh seung min <ohmin@gmail.com>
2024-02-25 23:24:19 +0900

    UseGuides 컴포넌트 적용 정상 작동 확인 완료

--- src/component/second-component.jsx
+++ src/component/second-component.jsx
@@ -1,30 +1,21 @@
-import React from 'react'
-import Description from './description';
+import React from "react";
+import UseGuides from "./useGuides.jsx";
 
-function secondContainer({handleClick, inputText, handleChange, handleKeyPress}) {
+function secondContainer({
+  handleClick,
+  inputText,
+  handleChange,
+  handleKeyPress,
+}) {
   return (
     <div className="second-container">
-    <button id="text-name" onClick={handleClick}>
-      Hint
-    </button>
-    {/* onkeyPress */}
-    <input
-      type="text"
-      id="search-bar"
-      name="search-bar"
-      placeholder="입력해주세요."
-      value={inputText}
-      onChange={handleChange}
-      onKeyDown={handleKeyPress}
-    />
-    {/* 컴포넌트 한번 더 */}
-    <div className="use-guides">
-      <div className="memoHeaderColor">
-        <h3 className="padding-10">사용 가이드</h3>
-      </div>
-      <Description />
+      <button id="text-name" onClick={handleClick}>
+        Hint
+      </button>
+      {/* onkeyPress */}
+      <input type="text" id="search-bar" name="search-bar" placeholder="입력해주세요." value={inputText} onChange={handleChange} onKeyDown={handleKeyPress} />
+      <UseGuides />
     </div>
-  </div>
   );
 }
 
