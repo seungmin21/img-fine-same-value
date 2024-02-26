5c4d2bfa9903d0412da968a08cab01907fd2f9b4
ohseungmin <ohmin2886@gmail.com>
2024-02-26 09:35:54 +0900

    DIv 컴포넌트 만들어는 실험 진행

--- src/component/recordShowPlaces.jsx
+++ src/component/recordShowPlaces.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import IconTitle from './iconTitle.jsx'
-import Div from './Div.jsx'
 
 function recordShowPlaces ( {logData} ) {
   return (
@@ -9,10 +8,9 @@ function recordShowPlaces ( {logData} ) {
           <IconTitle />
           <hr />
           {logData.map((item, index) => (
-            //<div className="marginTop-20 marginLeft-16" key={index}>
-            //  {item}
-            //</div>
-            <Div marginTop={20} marginLeft={16} key={index}>{item}</Div>
+            <div className="marginTop-20 marginLeft-16" key={index}>
+              {item}
+            </div>
           ))}
         </div>
       </div>
